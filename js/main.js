6d4d7b44f3c7f4a31bf5182d88139a0e291bc55d
musan63 <i151336@gm.ishikawa-nct.ac.jp>
2016-12-21 13:56:36 +0900

    add - css, change - html file

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
-var item = { "pencil" : 0, "elacer" : 0, "core" : 0 };
+var stationery = { "pencil" : 0, "eraser" : 0, "core" : 0 };
+
+/*
+
+clickしたら下の関数を実行
+idを引数に取りたい。取得方法？
+idと変数名をイコールにすれば，足し算はでき，どう返せばいいのか
+
+var plus = function(item) {
+  stationery.item++;
+  $(#item).text(stationery.item);
+};
+
+*/
+
 
 $(document).ready( function() {
 
@@ -7,16 +21,44 @@ $(document).ready( function() {
   // pencilに上位を設けて，その下位でプラスマイナスの処理，それらが終わった跡に表示
 
   $("#pencil_p").click(function(){
-    item.pencil++;
-    $("#pencil_a").text(item.pencil);
+    stationery.pencil++;
+    $("#pencil_a").text(stationery.pencil);
   });
   $("#pencil_d").click(function(){
-    item.pencil--;
-    if(item.pencil > 0)
-      $("#pencil_a").text(item.pencil);
+    stationery.pencil--;
+    if(stationery.pencil > 0)
+      $("#pencil_a").text(stationery.pencil);
     else{
-      item.pencil = 0;
-      $("#pencil_a").text(item.pencil);
+      stationery.pencil = 0;
+      $("#pencil_a").text(stationery.pencil);
+    }
+  });
+
+  $("#eraser_p").click(function(){
+    stationery.eraser++;
+    $("#eraser_a").text(stationery.eraser);
+  });
+  $("#eraser_d").click(function(){
+    stationery.eraser--;
+    if(stationery.eraser > 0)
+      $("#eraser_a").text(stationery.eraser);
+    else{
+      stationery.eraser = 0;
+      $("#eraser_a").text(stationery.eraser);
+    }
+  });
+
+  $("#core_p").click(function(){
+    stationery.core++;
+    $("#core_a").text(stationery.core);
+  });
+  $("#core_d").click(function(){
+    stationery.core--;
+    if(stationery.core > 0)
+      $("#core_a").text(stationery.core);
+    else{
+      stationery.core = 0;
+      $("#core_a").text(stationery.core);
     }
   });
 });
